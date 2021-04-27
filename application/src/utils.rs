a8df73b845c9c7631ced6eda5a201ea22d9a39be
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2021-04-26 20:53:57 -0400

    Beautified and fixed Clippy warning

--- application/src/utils.rs
+++ application/src/utils.rs
@@ -1,3 +1,6 @@
-pub fn rounded_integer_division(dividend: usize, divisor: usize) -> usize {
-    return (dividend + (divisor / 2)) / divisor;
+pub fn rounded_integer_division(
+    dividend: usize,
+    divisor:  usize,
+) -> usize {
+    (dividend + (divisor / 2)) / divisor
 }
